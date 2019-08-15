454f8bd4b1cae9fc86a088b08b4fbff398f72a09
Attila Orosz <atlantic.omnibus@gmail.com>
2019-08-15 12:22:50 +0200

    Added license info, and removed unnecessary/leftover files

--- app/src/main/java/com/atlanticomnibus/controlsheet/WrappingViewPager.java
+++ app/src/main/java/com/atlanticomnibus/controlsheet/WrappingViewPager.java
@@ -1,4 +1,3 @@
-package com.atlanticomnibus.controlsheet;
 /*
  * ControlSheet
  *
@@ -20,6 +19,7 @@ package com.atlanticomnibus.controlsheet;
  * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
  * THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+package com.atlanticomnibus.controlsheet;
 
 import android.content.Context;
 import android.util.AttributeSet;
